6e59fd71280917ecefc897893a3e497421fcc186
paula milani <paula.millanii@gmail.com>
2019-08-20 14:55:02 -0300

    Update specLogin.js

--- spec/specLogin.js
+++ spec/specLogin.js
@@ -1,11 +1,11 @@
 // specLogin.js
 describe('Login', function () {
   it('login', function () {
-    browser.get('https://gestaocomercial.pagseguro.com.br');
+    browser.get('url');
     //interargir com elementos da página
     var button = element(by.id('btnChoosePagseguro')).click();
-    var login = element(by.id('login')).sendKeys('sf_gcanais');
-    var password = element(by.id('password')).sendKeys('DresoV68o');
+    var login = element(by.id('login')).sendKeys('user');
+    var password = element(by.id('password')).sendKeys('senha');
     var button = element(by.id('btnEntrar')).click();
     var menuSair = element(by.css('[data-ng-click="menuController.logout()"]'));
 
