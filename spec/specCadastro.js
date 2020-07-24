fc8649918feea228ca5f1c000d232d578b925bdc
paula milani <paula.millanii@gmail.com>
2020-07-24 08:56:09 -0300

    Update and rename specCadastroParceiro.js to specCadastro.js

--- spec/specCadastro.js
+++ spec/specCadastro.js
@@ -1,23 +1,23 @@
-//specCadastroParceiro
-describe('Cadastro de Parceiro', function () {
-    it('cadastro novo parceiro ', function () {
+//specCadastro
+describe('Cadastro', function () {
+    it('cadastro novo', function () {
         var EC = protractor.ExpectedConditions;
-        var menuCadastroParceiro = element(by.id('partner')).click();
-        browser.wait(EC.elementToBeClickable(menuCadastroParceiro), 5000);
-        var novoParceiro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
+        var menuCadastro = element(by.id('partner')).click();
+        browser.wait(EC.elementToBeClickable(menuCadastro), 5000);
+        var novoCadastro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
         var cnpj = element(by.id('cnpj')).sendKeys('31.347.300/0001-75');
         var name = element(by.id('name')).sendKeys('Parceiro Teste um');
         var email = element(by.id('email')).sendKeys('parceiro.teste@oi.com');
         //var modeloNegocio = findElement(protractor.By.css('select option:2')).click();
         var btnSalvar = element(by.css('[data-ng-click="partnerCreateController.onSave()"]')).click();
         
-        //expect(getMessageModal()).toEqual('Parceiro criado com sucesso!');
+        //expect(getMessageModal()).toEqual('Cadastro criado com sucesso!');
     });
 
 
-    it('cadastro parceiro já existente ', function () {
-        var menuCadastroParceiro = element(by.id('partner')).click();
-        var novoParceiro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
+    it('cadastro já existente ', function () {
+        var menuCadastro = element(by.id('partner')).click();
+        var novoCadastro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
         var cnpj = element(by.id('cnpj')).sendKeys('31.347.300/0001-75');
         var name = element(by.id('name')).sendKeys('Parceiro Teste dois');
         var email = element(by.id('email')).sendKeys('parceiro1.teste@oi.com');
@@ -27,9 +27,9 @@ describe('Cadastro de Parceiro', function () {
         // expect(page.getText()).toEqual('Já existe um parceiro cadastrado com o mesmo CNPJ e modelo de negócio.')
     });
 
-    it('cadastro parceiro  botao cancelar', function () {
-        var menuCadastroParceiro = element(by.id('partner')).click();
-        var novoParceiro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
+    it('cadastro cancelado', function () {
+        var menuCadastro = element(by.id('partner')).click();
+        var novoCadastro = element(by.css('[data-ng-click="partnerSearchController.goTo('+"'partner.create'"+')"]')).click();
         var btnCancelar = element(by.css('[data-ng-click="partnerCreateController.onCancel()"]')).click();
 
         //expect(page.getText()).toEqual('Já existe um parceiro cadastrado com o mesmo CNPJ e modelo de negócio.')
